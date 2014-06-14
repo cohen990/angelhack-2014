cb90db822a63d7ec13639d7b720d300b57a284ac
Bryan Yap <bryan.yap@alliants.com>
2014-06-14 20:35:41 +0100

    add basic in-page routing

--- angular-app/app/js/app.js
+++ angular-app/app/js/app.js
@@ -1,5 +1,10 @@
 (function() {
   'use strict';
 
-  window.App = angular.module('App', ['Users', 'mobile-angular-ui']);
+  window.App = angular.module('App', ['Users', 'mobile-angular-ui', 'ngRoute']);
+
+  window.App.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', { templateUrl: 'templates/new-game.html' });
+    $routeProvider.when('/search', { templateUrl: 'templates/search-games.html' });
+  });
 })();
