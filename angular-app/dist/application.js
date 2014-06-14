541ea7aff8a2599ccb28558ab9eb3d176f2d0a47
Bryan Yap <bryan.yap@alliants.com>
2014-06-14 19:44:57 +0100

    black-cards now displays something from the database

--- angular-app/dist/application.js
+++ angular-app/dist/application.js
@@ -11,14 +11,28 @@
 
   Users.service('UsersService', [
     function() {
-      this.username = '';
-      this.cards = Get3BlackCards();
+      var self = this;
+      self.observers = [];
+
+      self.addObserver = function(observer) {
+        if (self.observers.indexOf(observer) === -1) {
+          self.observers.push(observer);
+        }
+      };
+
+      Get3BlackCards(function(data) {
+        for (var i = 0; i < self.observers.length; i++) {
+          self.observers[i].$apply(function(scope) {
+            scope.cards = data;
+          });
+        }
+      });
   }]);
 
   Users.controller('UsersController', ['$scope', 'UsersService',
     function($scope, UsersService) {
-      $scope.cards = UsersService.cards;
-
+      $scope.cards = [];
+      UsersService.addObserver($scope);
       $scope.selectCard = function(id) {
         init(id, 'user');
       }
