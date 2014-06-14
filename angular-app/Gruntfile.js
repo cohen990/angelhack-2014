581f284c3e034bd5d8993057268b65344d67b2b4
Bryan Yap <bryan.yap@alliants.com>
2014-06-14 15:44:55 +0100

    hooked up sample black-cards.html with db-api and card-session

--- angular-app/Gruntfile.js
+++ angular-app/Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -9,9 +11,29 @@ module.exports = function(grunt) {
         dest: 'dist/application.js'
       },
       css: {
-        src: ['app/css/**/*.css'],
+        src: ['tmp/cache/css/**/*.css'],
         dest: 'dist/application.css'
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'tmp/cache/css/cards.css': 'app/css/cards.scss'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['app/js/**/*.js'],
+        tasks: ['compile:js']
+      },
+      css: {
+        files: ['app/css/**/*.scss'],
+        tasks: ['compile:css']
       }
     }
   });
+
+  grunt.registerTask('compile:css', ['sass:dist', 'concat:css']);
+  grunt.registerTask('compile:js', ['concat:js']);
 };
