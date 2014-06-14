b602c973ba25464bceeda1f34cd1730298eeee2f
bovidiu <bovidiu87@gmail.com>
2014-06-14 13:13:18 +0100

    Codeingiter core

---
+++ app/application/views/inc/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OSb Web Solutions</title>
+
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Start Bootstrap - SB Admin Version 2.0 Demo</title>
+
+    <!-- Core CSS - Include with every page -->
+    <link href="<?php echo base_url(); ?>public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/font-awesome/css/font-awesome.css" rel="stylesheet">
+    
+     <!-- Page-Level Plugin CSS - Dashboard -->
+    <link href="<?php echo base_url(); ?>public/css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/css/plugins/timeline/timeline.css" rel="stylesheet">
+    
+
+    <!-- SB Admin CSS - Include with every page -->
+    <link href="<?php echo base_url(); ?>public/css/sb-admin.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/css/general.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/shadowbox/shadowbox.css" rel="stylesheet">
+    
+    <script type="text/javascript" src="<?php echo  base_url(); ?>public/js/jquery-1.10.2.js"></script>
+    <script type="text/javascript" src="<?php echo  base_url(); ?>public/shadowbox/shadowbox.js"></script>
+    <script type="text/javascript">
+      Shadowbox.init();
+    </script>
+  </head>
+
+  <body>
+
+
+      
---
+++ app/application/views/welcome_message.php
@@ -0,0 +1,26 @@
+
+<?php include 'inc/header.inc'; ?>
+<?php include 'inc/sidebar.inc'; ?>
+ <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+
+
+                    <form role="form">
+                        <div class="form-group">
+                            <label>L2 Devoted Servers</label>
+                            <input class="form-control">
+                            <p class="help-block">Example block-level help text here.</p>
+                        </div>
+                        <button type="submit" class="btn btn-default">Search</button>
+                    </form>
+
+
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+    </div>
+    <!-- /#wrapper -->
+
+<?php include 'inc/footer.inc'; ?>
